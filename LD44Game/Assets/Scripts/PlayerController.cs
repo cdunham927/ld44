dec82ba66f17b0466592e54d15d040ad30446f70
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2019-04-26 23:17:13 -0700

    Made A Player and Turn Controller

---
+++ LD44Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float hp;
+    public float maxHp;
+    public TurnController turnController;
+    
+    public void Attack ()
+    {
+        if (turnController.player_turn == true)
+        {
+            turnController.player_turn = false;
+        }
+    }
+    void Update()
+    {
+        Attack();
+    }
+}
