4175abf20df566ff33a969295517b3ac02e041b1
cdunham927 <cdunham927@gmail.com>
2019-04-28 13:35:31 -0700

    Merging

--- LD44Game/Assets/Scripts/PlayerController.cs
+++ LD44Game/Assets/Scripts/PlayerController.cs
@@ -91,7 +91,7 @@ public class PlayerController : MonoBehaviour
     {
         if (hand.Count < maxHand)
         {
-            if (enemy.hand.Count == 0;)
+            if (enemy.hand.Count != 0)
             {
             int x = Random.Range(0, enemy.hand.Count);
             enemy.hand[x].gameObject.transform.SetParent(playerDeck.transform);
