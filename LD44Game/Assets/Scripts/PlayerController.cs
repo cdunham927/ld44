ab43931e13e4792d94a5735971b83fe39513779b
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2019-04-28 13:32:56 -0700

    Added More Buttons

--- LD44Game/Assets/Scripts/PlayerController.cs
+++ LD44Game/Assets/Scripts/PlayerController.cs
@@ -78,7 +78,7 @@ public class PlayerController : MonoBehaviour
     public void Bigger ()
     {
         maxHand += 1;
-        hp -= 15;
+        hp -= 10;
     }
 
     public void PowerUp ()
@@ -91,7 +91,7 @@ public class PlayerController : MonoBehaviour
     {
         if (hand.Count < maxHand)
         {
-            if (enemy.hand.Count == 0;)
+            if (enemy.hand.Count > 0)
             {
             int x = Random.Range(0, enemy.hand.Count);
             enemy.hand[x].gameObject.transform.SetParent(playerDeck.transform);
@@ -103,7 +103,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void SeeHand ()
+    public void Spy ()
     {
         int x = Random.Range(0, hand.Count);
         enemy.hand[x].show = true;
